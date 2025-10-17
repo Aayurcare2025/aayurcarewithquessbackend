f09b0b39eee4bd67a34cfea295379afa081ce65c
info@aayurcare.com <info@aayurcare.com>
2025-10-17 10:58:14 +0530

    push code

--- src/main.ts
+++ src/main.ts
@@ -34,7 +34,7 @@ async function bootstrap() {
   app.use(bodyParser.json({ limit: '50mb' }));
   app.use(bodyParser.urlencoded({ limit: '50mb', extended: true }));
 
-  await app.listen(7000, '0.0.0.0');
+  await app.listen(6000, '0.0.0.0');
 }
 
 bootstrap();
